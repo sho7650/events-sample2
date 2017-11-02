6dbb7a2d10994b8f34811603caef860b39d8dedc
Takashi Abe <tabe@salesforce.com>
2017-11-02 18:37:11 +0900

    Added publishing multiple messages.

--- publisher.js
+++ publisher.js
@@ -1,3 +1,7 @@
+/*
+ * publisher.js
+ */
+
 require('dotenv').config();
 var jsforce = require('jsforce');
 
@@ -5,10 +9,21 @@ var conn = new jsforce.Connection({
   loginUrl : 'https://login.salesforce.com'
 });
 
+// login to salesforce
+// TODO: adopt JWT
 conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, function(err, userInfo) {
   if (err) { return console.error(err); }
 
-  conn.sobject("Notification__e").create({Message__c: "Publish test"}, function(err, ret) {
-    if (err || !ret.success) { return console.error(err, ret); }
-  });
+  var msg = {Message__c: "Publish test"};
+  var i = 2;
+
+  do {
+    console.log(msg.Message__c);
+    if (process.argv.length > 2) { msg.Message__c = process.argv[i]; }
+
+    conn.sobject("Notification__e").create(msg, function(err, ret) {
+      if (err || !ret.success) { return console.error(err, ret); }
+    });
+  } while (++i < process.argv.length);
+
 });
