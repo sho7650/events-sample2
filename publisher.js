114b0b2c412bfdb8ff26faecb348287636ffa36a
Takashi Abe <tabe@salesforce.com>
2019-02-21 10:28:39 +0900

    first commit

--- publisher.js
+++ publisher.js
@@ -3,25 +3,28 @@
  */
 
 require('dotenv').config();
-var jsforce = require('jsforce');
+const jsforce = require('jsforce');
 
-var conn = new jsforce.Connection({
+const conn = new jsforce.Connection({
   loginUrl : 'https://login.salesforce.com'
 });
 
 // login to salesforce
 // TODO: adopt JWT
-conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, function(err, userInfo) {
+conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, (err, userInfo) => {
   if (err) { return console.error(err); }
 
-  var msg = {Message__c: "Publish test"};
-  var i = 2;
+  console.log('login successfully.');
+  console.log(JSON.stringify(userInfo));
+
+  let msg = {'Name': 'Publish test'};
+  let i = 2;
 
   do {
-    console.log(msg.Message__c);
-    if (process.argv.length > 2) { msg.Message__c = process.argv[i]; }
+    console.log(msg.Name);
+    if (process.argv.length > 2) { msg.Name = process.argv[i]; }
 
-    conn.sobject("Notification__e").create(msg, function(err, ret) {
+    conn.sobject("Account").create(msg, (err, ret) => {
       if (err || !ret.success) { return console.error(err, ret); }
     });
   } while (++i < process.argv.length);
--- subscriber.js
+++ subscriber.js
@@ -5,12 +5,14 @@ var conn = new jsforce.Connection({
   loginUrl : 'https://login.salesforce.com'
 });
 
-conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, function(err, userInfo) {
+conn.login(process.env.SFDC_USERNAME, process.env.SFDC_PASSWORD, (err, userInfo) => {
   if (err) { return console.error(err); }
-
+  
+  console.log('login successfully.');
+  console.log(JSON.stringify(userInfo));
   console.log('Start subscription');
 
-  conn.streaming.topic("/event/Notification__e").subscribe(function(message) {
+  conn.streaming.topic(process.env.TOPIC).subscribe((message) => {
    console.log('Received :' + JSON.stringify(message, null, 2));
   });
 
