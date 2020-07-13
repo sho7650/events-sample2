211beaa5925c6892453af4b22a31df3e5da4bddf
sho kisaragi <sho@oshiire.to>
2020-07-13 20:20:53 +0900

    same both messages

--- jwt.js
+++ jwt.js
@@ -1,41 +1,41 @@
-'use strict';
+"use strict";
 
 // libraries
-const jwt = require('jsonwebtoken');
-const jsforce = require('jsforce');
-const request = require('request');
+const jwt = require("jsonwebtoken");
+const jsforce = require("jsforce");
+const request = require("request");
 
 // .env
 // PRIVATE_KEY: private key by base64
 // ISSUER: consumer key from connected application
 // AUDIENCE: https://login.salesforce.com
 // SUBJECT: login user name like an e-mail address type
-require('dotenv').config();
+require("dotenv").config();
 
 // define
-const TOKEN_ENDPOINT_URL = 'https://login.salesforce.com/services/oauth2/token';
+const TOKEN_ENDPOINT_URL = "https://login.salesforce.com/services/oauth2/token";
 
 // sign
-const secret = Buffer.from(process.env.PRIVATE_KEY, 'base64');
+const secret = Buffer.from(process.env.PRIVATE_KEY, "base64");
 const claim = {
   iss: process.env.ISSUER,
   aud: process.env.AUDIENCE,
   sub: process.env.SUBJECT,
-  exp: Math.floor(Date.now() / 1000) + (3 * 60) // Salesforce の仕様上 5分未満を指定
+  exp: Math.floor(Date.now() / 1000) + 3 * 60, // Salesforce の仕様上 5分未満を指定
 };
 
-const token = jwt.sign(claim, secret, { algorithm: 'RS256' });
+const token = jwt.sign(claim, secret, { algorithm: "RS256" });
 
 // login salesforce with JWT
 exports.login = function (callback) {
   // prepare loging in
   const post = {
-    method: 'POST',
+    method: "POST",
     url: TOKEN_ENDPOINT_URL,
     form: {
-      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-      assertion: token
-    }
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+      assertion: token,
+    },
   };
 
   // request to login
@@ -50,9 +50,10 @@ exports.login = function (callback) {
 
     const conn = new jsforce.Connection({
       accessToken: ret.access_token,
-      instanceUrl: ret.instance_url
+      instanceUrl: ret.instance_url,
     });
-    console.log('login successfully.');
+    console.log("login successfully.");
+    console.log(conn);
 
     callback(conn);
   });
--- publisher.js
+++ publisher.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-
 const jwt = require("./jwt");
 
 jwt.login((conn) => {
-  let msg = { dummy__c: "Publish test" };
+  let msg = { dummy__c: "" };
 
   process.argv.slice(2, process.argv.length).forEach((arg) => {
     msg.dummy__c = arg;
@@ -11,8 +10,10 @@ jwt.login((conn) => {
     console.log(`event message = ${msg.dummy__c}`);
     conn.sobject(process.env.TOPIC).create(msg, (err, ret) => {
       if (err) {
+        console.log("error:");
         console.log(err);
       } else {
+        console.log("success:");
         console.log(ret);
       }
     });
