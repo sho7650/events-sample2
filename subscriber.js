ad6c063ca8a070e010d69a743c5194e8a1275f3a
sho kisaragi <sho@oshiire.to>
2020-07-13 13:31:00 +0900

    update and create subscriber using nforce

--- subscriber.js
+++ subscriber.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
-const jwt = require('./jwt');
+const jwt = require("./jwt");
+const jsforce = require("jsforce");
 
 jwt.login((conn) => {
-  console.log('Start subscription');
+  console.log("Start subscription");
 
-  conn.streaming.topic('/data/AccountChangeEvent').subscribe((message) => {
-    console.log(`Received : ${JSON.stringify(message, null, 2)}\n`);
+  const topic = `/event/${process.env.TOPIC}`;
+  const replayExt = new jsforce.StreamingExtension.Replay(topic, -2);
+  const fayeClient = conn.streaming.createClient([replayExt]);
+
+  console.log(`topic: ${topic}`);
+  console.log(fayeClient);
+
+  fayeClient.subscribe(topic, (message) => {
+    console.log("Event Type : " + message.event.type);
+    console.log("Event Created : " + message.event.createdDate);
+    console.log("Object Id : " + message.sobject.Id);
+    console.log(`Received: ${JSON.stringify(message, null, 2)}\n`);
   });
 });
